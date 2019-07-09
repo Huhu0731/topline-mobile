c22da1c3f4df6300c43df6c36a13b230fad77098
Huhu0731 <17634950228@163.com>
2019-07-09 10:23:08 +0800

    登陆--页面布局

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,25 @@
 <template>
   <div>
-    <p>login component</p>
+    <van-nav-bar title="登陆"/>
+    <form>
+      <van-cell-group>
+        <van-field
+          v-model="user.mobile"
+          clearable
+          label="手机号"
+          placeholder="请输入手机号"
+        />
+        <van-field
+          v-model="user.code"
+          type="password"
+          label="密码"
+          placeholder="请输入验证码"
+        />
+      </van-cell-group>
+    </form>
+    <div class="login-btn">
+      <van-button class="btn" type="info">登陆</van-button>
+    </div>
   </div>
 </template>
 
@@ -8,11 +27,21 @@
 export default {
   name: 'LoginIndex',
   data () {
-    return {}
+    return {
+      user: {
+        mobile: 17634950228,
+        code: 123456
+      }
+    }
   }
 }
 </script>
 
 <style lang="less" scoped>
-
+.login-btn {
+  padding: 10px;
+  .btn {
+    width: 100%;
+  }
+}
 </style>
