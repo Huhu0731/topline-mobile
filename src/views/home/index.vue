b0376ff892fd41327f72725ece9290188287e933
Huhu0731 <17634950228@163.com>
2019-07-12 10:35:59 +0800

    首页布局--基本实现-包含文章列表，下拉刷新-tabBar

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,8 +1,29 @@
 <template>
   <div>
-    <van-nav-bar
-      title="首页"
-    />
+    <van-nav-bar title="首页"/>
+    <van-tabs v-model="active">
+      <van-tab title="标签 1">
+        <!-- 下拉刷新 -->
+        <van-pull-refresh v-model="isLoading" @refresh="onRefresh">
+          <!-- 文章列表 -->
+          <van-list
+            v-model="loading"
+            :finished="finished"
+            finished-text="没有更多了"
+            @load="onLoad"
+          >
+            <van-cell
+              v-for="item in list"
+              :key="item"
+              :title="item"
+            />
+          </van-list>
+        </van-pull-refresh>
+      </van-tab>
+      <van-tab title="标签 2">内容 2</van-tab>
+      <van-tab title="标签 3">内容 3</van-tab>
+      <van-tab title="标签 4">内容 4</van-tab>
+    </van-tabs>
   </div>
 </template>
 
@@ -10,7 +31,38 @@
 export default {
   name: 'HomeIndex',
   data () {
-    return {}
+    return {
+      active: 0,
+      list: [],
+      loading: false,
+      finished: false,
+      isLoading: false
+    }
+  },
+  methods: {
+    // 加载文章列表事件
+    onLoad () {
+      // 异步更新数据
+      setTimeout(() => {
+        for (let i = 0; i < 10; i++) {
+          this.list.push(this.list.length + 1)
+        }
+        // 加载状态结束
+        this.loading = false
+
+        // 数据全部加载完成
+        if (this.list.length >= 40) {
+          this.finished = true
+        }
+      }, 500)
+    },
+    // 下拉刷新出发的事件
+    onRefresh () {
+      setTimeout(() => {
+        this.$toast('刷新成功')
+        this.isLoading = false
+      }, 500)
+    }
   }
 }
 </script>
