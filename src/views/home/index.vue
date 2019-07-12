f2b40b6cfa23a43ccca1296d7ec0fa7f443a86e4
Huhu0731 <17634950228@163.com>
2019-07-12 11:02:51 +0800

    首页布局-处理头部和频道导航固定

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <van-nav-bar title="首页"/>
-    <van-tabs v-model="active">
+    <van-nav-bar title="首页" fixed/>
+    <van-tabs v-model="activeChannelIndex" class="channel-tabs">
       <van-tab title="标签 1">
         <!-- 下拉刷新 -->
         <van-pull-refresh v-model="isLoading" @refresh="onRefresh">
@@ -32,7 +32,7 @@ export default {
   name: 'HomeIndex',
   data () {
     return {
-      active: 0,
+      activeChannelIndex: 0, // 当前频道的激活页
       list: [],
       loading: false,
       finished: false,
@@ -69,4 +69,13 @@ export default {
 
 <style lang="less" scoped>
 
+.channel-tabs {
+  margin-top: 100px;
+  margin-bottom: 100px;
+}
+// 深度作用选择器：https://vue-loader.vuejs.org/zh/guide/scoped-css.html#%E6%B7%B1%E5%BA%A6%E4%BD%9C%E7%94%A8%E9%80%89%E6%8B%A9%E5%99%A8
+.channel-tabs /deep/ .van-tabs__wrap {
+  position: fixed;
+  top: 92px;
+}
 </style>
